8d9757039bef8777bb2a25426ea31df927843637
Abhishek Anand <ab.work2103@gmail.com>
2026-07-28 16:19:25 +0530

    add score update function in the project

--- coinFlipGame/index.js
+++ coinFlipGame/index.js
@@ -1,9 +1,10 @@
 const playerMove = document.querySelector(".player-move");
 const computerMove = document.querySelector(".computer-move");
 const resultPara = document.querySelector(".result-para");
+const tieScore = document.querySelector(".tie-score")
 const playerScore = document.querySelector(".player-score");
 const computerScore = document.querySelector(".computer-score");
-const resetButton = document.querySelector(".reset-btn");
+const restartButton = document.querySelector(".restart-btn");
 
 // Mine code. I was able to build logic but failed to pass data from one function to another function.
 /*
@@ -54,24 +55,41 @@ function computerPlay() {
   return Math.random() < 0.5 ? "Heads" : "Tails";
 }
 
+let tie = 0;
+let playerWins = 0;
+let computerWins = 0;
+
 function result(playerCoin, computerCoin) {
+
   if (playerCoin === computerCoin) {
     resultPara.textContent = `Player choose ${playerCoin} & Computer choose ${computerCoin}, game is tie.`;
+
+    tie++;
+    tieScore.textContent = `Tie: ${tie}`;
     
   } else if (playerCoin === "Heads" && computerCoin === "Tails") {
     resultPara.textContent = `Player choose ${playerCoin} & Computer choose ${computerCoin}, Player wins.`;
+
+    playerWins++;
+    playerScore.textContent = `Player: ${playerWins}`
     
   } else {
     resultPara.textContent = `Player choose ${playerCoin} & Computer choose ${computerCoin}, Computer wins.`;
+
+    computerWins++;
+    computerScore.textContent = `Computer: ${computerWins}`
   }
 };
 
 playerPlay();
 
-function resetBtn() {
-  resetButton.addEventListener("click", () => {
+function restartBtn() {
+  restartButton.addEventListener("click", () => {
     resultPara.textContent = "";
+    tieScore.textContent= "";
+    playerScore.textContent= "";
+    computerScore.textContent= "";
   })
 };
 
-resetBtn();
+restartBtn();
